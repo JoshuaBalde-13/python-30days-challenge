5fd11f786a1dd3897f0c754f2cdf546ab15c7494
Joshua Balde <baldebryanjoshua13@gmail.com>
2026-08-25 23:59:17 +0800

    Day 01: Completed

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -2,9 +2,52 @@
 # Write your solutions below.
 
 # ---- Exercise 1 ----
+employee_name = "Joshua"
+age = 20
+salary = 50000.89
+is_active = True
 
+print(type(employee_name))
+print(type(age))
+print(type(salary))
+print(type(is_active))
 
 # ---- Exercise 2 ----
+# 1.
+print(len(employee_name))
 
+# 2.
+age_text = str(age)
+print(employee_name + " is " + age_text + " years old" )
+# or
+print(f"{employee_name} is {age} years old")
+
+# 3.
+salary_int = int(salary)
+print(salary_int) #This converted to a number only without a decimal 
+
+
+# 4. 
+print(f"Active status: {is_active}")
 
 # ---- Exercise 3 ----
+# 1.
+
+age_input = input("Enter your age: ")
+
+# 2.
+
+age_converted = int(age_input)
+print(age_converted)
+
+# 3.
+
+print(type(age_input))
+print(type(age_converted))
+
+# 4.
+
+year_calculated = 2026 - age_converted
+result = year_calculated
+print(result)
+
